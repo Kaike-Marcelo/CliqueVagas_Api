6fed96d40d485e9aec027c79fddf901d9f711613
Kaike-Marcelo <kaikeemarceloo@gmail.com>
2025-01-11 12:23:13 -0300

    Projeto Base

---
+++ clique-vagas-api/src/main/java/com/pi/clique_vagas_api/CliqueVagasApiApplication.java
@@ -0,0 +1,13 @@
+package com.pi.clique_vagas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliqueVagasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliqueVagasApiApplication.class, args);
+	}
+
+}
---
+++ clique-vagas-api/src/test/java/com/pi/clique_vagas_api/CliqueVagasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.clique_vagas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliqueVagasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
